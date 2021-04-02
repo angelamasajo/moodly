d70248fae5f5ae85af243ed1ee44f48ba987cab8
Angela Masajo <aabmasajo@gmail.com>
2021-04-02 00:47:39 -0500

    api endpoint testing

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  API_ENDPOINT: "https://guarded-retreat-20421.herokuapp.com/api",
+  // API_ENDPOINT: "https://guarded-retreat-20421.herokuapp.com/api",
+  API_ENDPOINT: "http://localhost:8000/api"
 };
 
 export default config;
