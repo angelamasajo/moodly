1b577673cf702c91318358d253d0c9da412f0593
Angela Masajo <aabmasajo@gmail.com>
2021-04-03 14:00:34 -0500

    tests working

--- src/EntryListItem/EntryListItem.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import EntryListItem from "./EntryListItem";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<EntryListItem />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
